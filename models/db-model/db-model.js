aacefc8649ccd25348b1bcbcdbe7f5e96b1c7e1a
serhii-123 <plotva10@gmail.com>
2023-04-29 20:08:35 +0300

    Some commit

---
+++ models/db-model/db-model.js
@@ -0,0 +1,12 @@
+const mysql = require('mysql2');
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    database: 'shop',
+    password: 'Pisunmakaki123',
+    waitForConnections: true,
+    connectionLimit: 10,
+    queryLimit: 0,
+});
+
+module.exports = pool;
---
+++ sql/db.sql
@@ -0,0 +1,42 @@
+create database shop;
+use shop;
+
+create table payment (
+	id int auto_increment primary key,
+    card_number text,
+    card_date text,
+    card_cvv int
+);
+
+create table `order` (
+	id int auto_increment,
+    `name` text,
+    surname text,
+    patronymic text,
+    phone_number text,
+    payment_id int,
+    primary key (id),
+	foreign key (payment_id) references payment(id)
+);
+
+create table inventory (
+	id int auto_increment primary key,
+    quantity int
+);
+
+create table product(
+	id int auto_increment,
+    `name` text,
+    `description` text,
+    category text,
+    image_name text,
+    inventory_id int,
+    primary key (id),
+    foreign key (inventory_id) references inventory (id)
+);
+
+create table admin (
+	id int auto_increment primary key,
+    username text,
+    `password` text
+);
