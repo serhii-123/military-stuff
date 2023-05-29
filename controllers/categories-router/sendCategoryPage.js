75c2644bbee1ce40f27286f5496094bf527f3f5b
serhii-123 <plotva10@gmail.com>
2023-05-29 20:37:57 +0300

    Some commit

--- controllers/categories-router/sendCategoryPage.js
+++ controllers/categories-router/sendCategoryPage.js
@@ -4,7 +4,6 @@ module.exports = async function(req, res) {
     let category = req._parsedUrl.path.substring(1);
     let products = await getProductsByCategory(category);
     
-    console.log(products);
     res.render('category.hbs', {
         products
     });
