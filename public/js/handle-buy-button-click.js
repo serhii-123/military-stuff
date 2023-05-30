d7aff20d6d0083d774d8b04951d6169605f0b56a
serhii-123 <plotva10@gmail.com>
2023-05-30 20:54:43 +0300

    Some commit

--- public/js/handle-buy-button-click.js
+++ public/js/handle-buy-button-click.js
@@ -6,12 +6,14 @@ btn.addEventListener('click', async () => {
     
     let urlParts = window.location.href.split('/');
     let productId = urlParts.pop();
-    console.log(productId);
+
     let response = await fetch(`http://localhost/api/product-quantity?id=${productId}`);
     let data = await response.json();
     let quantity = data.quantity;
 
     if(value > quantity) {
         alert('Помилка! На складі недостатньо товарів');
+    } else {
+        window.location.href = `http://localhost/buy?id=${productId}&quantity=${quantity}`;
     }
 });
